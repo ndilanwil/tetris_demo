fd4aa884179efe49ee66c2aece012b04d1dac167
ndilanwil <nuadjedidi@gmail.com>
2022-09-22 14:12:28 +0100

    uses the constants declared and actually create tetrominos randomly

--- tetrominos.js
+++ tetrominos.js
@@ -17,7 +17,7 @@ class Piece {
         this.ctx.fillStyle = this.color;
         this.shape.forEach((row, y) => {
             row.forEach((value, x) => {
-                if(value > 0) {
+                if (value > 0) {
                     this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
                 }
             });
@@ -40,7 +40,7 @@ class Piece {
         this.x = this.typeId === 4 ? 4 : 3;
     }
 
-    randomizeTetrominoType(noOfTypes){
+    randomizeTetrominoType(noOfTypes) {
         return Math.floor(Math.random() * noOfTypes + 1);
     }
 }
